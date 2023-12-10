062d1c6f0bb8ec167677de02c42688be68d08344
ArhipSizov <arhip.sizov111@gmail.com>
2023-12-10 23:21:20 +0300

    Egor_,I_do_not_impotant_css!

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
 import { useState } from 'react'
+import MenuBlock from "./Menu/Menu"
 import './App.css'
+import arrCard from "./ListBurgers.json"
 
 function App() {
-  
+
+  const MenuList = arrCard.Menu
+
+  const arrCards = arrCard.allUsers
+
+  const [menu] = useState(MenuList)
+  if (!menu){
+    return <h1>Eror404</h1>
+  }
+
+  const [burgers] = useState(arrCards)
+  if (!burgers){
+    return <h1>Eror404</h1>
+  }
+
+  console.log(menu);
+
 
   return (
-    <div className="HELL"></div>
+    <div className="All">
+      <div className="main">
+      {menu.map(item => <MenuBlock {...item} key={item.id}/>)}
+      </div>
+      <p className='name_of_menu'>Бургеры</p>
+    </div>
   )
 }
 
---
+++ src/Menu/Menu.jsx
@@ -0,0 +1,12 @@
+import './Menu.css';
+
+export default function MenuBlock({img, text, menu_div}) {
+
+    console.log(img);
+    return(
+        <div className={menu_div}>
+            <img className='menu_img' src={img} alt="" />
+            <p className="menu_text">{text}</p>
+        </div>
+    )
+}
