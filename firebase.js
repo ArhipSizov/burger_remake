b008cd5bfe23db9394b2e471f1e54f961d633f31
ArhipSizov <arhip.sizov111@gmail.com>
2024-01-30 15:43:53 +0300

    firebase v1

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC_4em4swdDqRUvjDC8mJCAY-iX2QpkKL4",
+  authDomain: "burgers-new.firebaseapp.com",
+  projectId: "burgers-new",
+  storageBucket: "burgers-new.appspot.com",
+  messagingSenderId: "190162556826",
+  appId: "1:190162556826:web:1ef72d54ef775d7f100c23"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
